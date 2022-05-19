b62937fc777e8a4ce48860e9269ece03a4e667a9
jiniux <53437303+jiniux@users.noreply.github.com>
2022-05-19 11:37:18 +0200

    Add example

---
+++ Doron.Examples/Program.cs
@@ -0,0 +1,41 @@
+﻿using Doron;
+using Doron.Connections;
+using Doron.Extensions;
+
+Console.WriteLine("Doron example - echo server");
+Server server = new Server(3000);
+
+async Task HandleConnection(WebSocketConnection connection)
+{
+    using (connection)
+    {
+        while (true)
+        {
+            try
+            {
+                var actionResult = await connection.ReceiveMessageAsync().RunWithTimeout(10000);
+
+                if (actionResult.Status != WebSocketConnection.WebSocketActionStatus.Ok)
+                    break;
+
+                if (actionResult.Result is WebSocketMessage.Text or WebSocketMessage.Binary)
+                    await connection.SendMessageAsync(actionResult.Result!);
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
+
+async Task AcceptConnections()
+{
+    while (true)
+    {
+        WebSocketConnection connection = await server.AcceptConnectionAsync();
+        _ = HandleConnection(connection);
+    }
+}
+
+await Task.WhenAny(server.RunAsync(), AcceptConnections());
